dc349e265b1e1653a917007c1dbb170211e7384d
vlad-outscraper <service@outscraper.com>
2022-07-22 21:21:19 +0300

    add search v3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='outscraper',
-    version='1.6.1',
+    version='1.7.0',
     description='Python bindings for the Outscraper API',
     long_description=readme(),
     classifiers = ['Programming Language :: Python',
