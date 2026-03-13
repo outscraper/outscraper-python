136af059ba8c3b81478bf7f1a461e61e84bd289d
Stepbus <73597994+Stepbus@users.noreply.github.com>
2026-03-13 09:48:52 +0200

    Enrichments (#37)
    * add enrichments; version increased
    
    * user query structure changed
    
    * changed readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='outscraper',
-    version='6.0.2',
+    version='6.0.3',
     description='Python bindings for the Outscraper API',
     long_description=readme(),
     classifiers = ['Programming Language :: Python',
